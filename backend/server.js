4c59d7998cd4d67c3fbceb61f4031b5f8801608d
mtbishop <bishopmatthew76@gmail.com>
2021-11-12 21:49:46 -0700

    heroku build

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const { errorHandler, notFound } = require('./middlewares/errorMiddleware');
+const path = require('path');
 
 const app = express();
 dotenv.config();
@@ -12,6 +13,19 @@ app.use(express.json());
 const PORT = process.env.PORT || 5001;
 
 app.use('/api/users', userRoutes);
+
+//deployment
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API Connected');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
